36996f18c81494677e118c2d36ff4dee5c2edf82
Kostis Gourgoulias <kostis.gourgoulias@babylonhealth.com>
2021-12-31 20:40:30 +0200

    more updates

---
+++ run_maze.py
@@ -0,0 +1,63 @@
+import numpy as np
+from pymaze.maze_manager import MazeManager
+from pymaze.maze import Maze
+from itertools import product
+from collections import defaultdict
+from simulator.particle import create_particle
+from simulator.simulator import simulate
+
+import matplotlib.pyplot as plt
+from celluloid import Camera
+
+manager = MazeManager()
+
+# We can add mazes to the manager two different ways.
+# The first way, we specify the maze dimensions. The maze that is created gets returned back to you.
+
+WIDTH = 10
+HEIGHT = 10
+
+maze = manager.add_maze(WIDTH, HEIGHT)
+
+# construct initial grid
+particles = defaultdict(lambda x=None: None)
+links = defaultdict(list)
+
+mapping = {
+    "top": np.array([0, 1]),
+    "bottom": np.array([0, -1]),
+    "left": np.array([-1, 0]),
+    "right": np.array([1, 0]),
+}
+
+
+# TODO fix this so that it is plottable.
+
+
+for i, j in product(range(WIDTH), range(HEIGHT)):
+    particles[(i, j)] = create_particle([i, j])
+    for wall, exists in maze.grid[i][j].walls.items():
+        if exists:
+            new_link = tuple(mapping[wall] + np.array([i, j]))
+            if min(new_link) < 0 or max(new_link) > (WIDTH - 1):
+                continue
+
+            links[(i, j)].append(new_link)
+
+fig = plt.figure()
+camera = Camera(fig)
+
+# particles = {i: elem for i, elem in enumerate(particles.values())}
+
+
+def default_distance():
+    return defaultdict(lambda x=None: 0.01)
+
+
+initial_link_distances = defaultdict(default_distance)
+pinned_particles = {(0, 0): np.array([0.0, 10.0]), (4, 9): np.array([1000.0, 10.0])}
+
+
+simulate(particles, links, pinned_particles, initial_link_distances, camera)
+animation = camera.animate()
+plt.show()
---
+++ simulator/simulator.py
@@ -0,0 +1,34 @@
+from tqdm import tqdm
+
+from simulator.plotting_utils import draw_lines, draw_points
+from simulator.conditions import (
+    apply_boundary_condition,
+    apply_link_constraints,
+)
+
+
+def simulate(particles, links, PINNED_PARTICLES, initial_link_distances, camera):
+    NUMBER_OF_TIMESTEPS = 10
+    for _ in tqdm(range(NUMBER_OF_TIMESTEPS)):
+        # draw points
+        draw_points(particles)
+
+        # draw lines
+        draw_lines(particles, links)
+        camera.snap()
+
+        # particle loop
+        for i, p in enumerate(particles.values()):
+            p.update_location()
+
+            # check boundary constraint
+            apply_boundary_condition(p)
+
+        # link constraints
+        apply_link_constraints(
+            particles,
+            links,
+            initial_link_distances,
+            pinned_particles=PINNED_PARTICLES,
+            number_of_passes=20,
+        )
